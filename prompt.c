426f83d24efc527f0cf023614e408ce1a7bc4aef
Fernando Paredes <nano@fdp.io>
2014-04-08 13:55:19 -0700

    Make prompt platform-agnostic

--- prompt.c
+++ prompt.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+// Fake add_history command
+void add_history(char* unused) {}
+
+// Otherwise include the editline headers
+#else
+
 #include <editline/readline.h>
 #include <editline/history.h>
+
+#endif
 
 int main(int argc, char** argv) {
 
