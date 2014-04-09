133224d8cf24b914b16cbd33382576f495b98c64
Fernando Paredes <nano@fdp.io>
2014-04-09 13:18:21 -0700

    Implement (^ 2 2) == 4

--- parsing.c
+++ parsing.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "mpc.h"
 
 #ifdef _WIN32
@@ -34,6 +35,7 @@ long eval_op(long x, char* op, long y) {
   if (strcmp(op, "*") == 0) { return x * y; }
   if (strcmp(op, "/") == 0) { return x / y; }
   if (strcmp(op, "%") == 0) { return x % y; }
+  if (strcmp(op, "^") == 0) { return pow(x, y); }
   return 0;
 }
 
@@ -68,7 +70,7 @@ int main(int argc, char** argv) {
   mpca_lang(MPC_LANG_DEFAULT,
     "                                                     \
       number   : /-?[0-9]+/ ;                             \
-      operator : '+' | '-' | '*' | '/' | '%' ;            \
+      operator : '+' | '-' | '*' | '/' | '%' | '^' ;      \
       expr     : <number> | '(' <operator> <expr>+ ')' ;  \
       lispy    : /^/ <operator> <expr>+ /$/ ;             \
     ",
