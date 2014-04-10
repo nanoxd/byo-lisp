8fccbf59e85ce1084708fd56381db85fdeaa9679
Fernando Paredes <nano@fdp.io>
2014-04-09 22:14:31 -0700

    Ch 9: Read expressions

--- parsing.c
+++ parsing.c
@@ -101,6 +101,42 @@ void lval_del(lval* v) {
   free(v);
 }
 
+lval* lval_add(lval* v, lval* x) {
+  v->count++;
+  v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+  v->cell[v->count-1] = x;
+  return v;
+}
+
+lval* lval_read_num(mpc_ast_t* t) {
+  errno = 0;
+  long x = strtol(t->contents, NULL, 10);
+  return errno != ERANGE ? lval_num(x) :lval_err("invalid number");
+}
+
+lval* lval_read(mpc_ast_t* t) {
+  // If Symbol or Number return conversion to taht type
+  if (strstr(t->tag, "number")) { return lval_read_num(t); }
+  if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+
+  // If root (>) or sexpr then create empty list
+  lval* x = NULL;
+  if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+  if (strcmp(t->tag, "sexpr"))  { x = lval_sexpr(); }
+
+  // Fill list with any valid expression contained within
+  for (int i = 0; i < t->children_num; i++) {
+    if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, "}") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, "{") == 0) { continue; }
+    if (strcmp(t->children[i]->tag,  "regex") == 0) { continue; }
+    x = lval_add(x, lval_read(t->children[i]));
+  }
+
+  return x;
+}
+
 // Print an "lval"
 void lval_print(lval v) {
   switch (v.type) {
